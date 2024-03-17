7381f2a5399b180bb7b347c2b47f753ac5c011dd
Daniel Holt <holt.daniel.b@gmail.com>
2024-03-17 19:30:05 -0400

    database created and pool updated

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+-- create db called "character_generator"
+
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
@@ -5,5 +7,26 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR (80) UNIQUE NOT NULL
 );
+
+CREATE TABLE "characters" (
+    "id" SERIAL PRIMARY KEY,
+    "character_name" VARCHAR (15),
+    "class" VARCHAR,
+    "level" INT,
+    "background" VARCHAR,
+    "race" VARCHAR,
+    "alignment" VARCHAR,
+    "experience_points" BIGINT,
+    "Strength" INT,
+    "dexterity" INT,
+    "constitution" INT,
+    "intelligence" INT,
+    "wisdom" INT,
+    "charisma" INT,
+    "hit_points" INT,
+    "user_id" BIGINT
+);
+
