c3cbf48157e1b1f4da49ecdfbee896b97ac9a560
Daniel Holt <holt.daniel.b@gmail.com>
2024-03-19 20:35:15 -0400

    char get route only checks for user authorization

--- server/routes/character.router.js
+++ server/routes/character.router.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
+const { rejectUnauthenticated, } = require('../modules/authentication-middleware');
 
 router.get("/", (req, res) => {
-  const query = 
-  `SELECT * FROM "characters"`
-
+  console.log('char get route');
+  console.log('is authenticated?', req.isAuthenticated());
+  console.log('user', req.user);
+  let query = 
+  `SELECT * FROM "characters"
+  WHERE characters.user_id = $1;`;
   pool
-  .query(query)
+  .query(query, [req.user.id])
   .then((result) => {
     res.send(result.rows);
   })
